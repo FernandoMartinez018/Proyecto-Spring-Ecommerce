0530725e705cea9b81ddee511255ac7ee32cd603
Fernando <lmartinezm013@gmail.com>
2023-07-14 21:54:03 -0500

    funcionalidad boton guardar y mostrar productos admin

--- src/main/java/com/proyecto/ecommerce/controler/ProductoController.java
+++ src/main/java/com/proyecto/ecommerce/controler/ProductoController.java
@@ -1,16 +1,47 @@
 package com.proyecto.ecommerce.controler;
 
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.proyecto.ecommerce.model.Producto;
+import com.proyecto.ecommerce.model.Usuario;
+import com.proyecto.ecommerce.service.ProductoService;
+
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private ProductoService productoService;
+	
 
 	@GetMapping("")
-	public String show() {
-		
+	public String show(Model model) {	
+		model.addAttribute("productos", productoService.mostrar());
 		return "productos/show";
 	}
+	
+	@GetMapping("/create")
+	public String create() {
+		return "productos/create";
+	}
+	
+	@PostMapping("/save")
+	public String guardar(Producto producto) {
+		LOGGER.info("Objeto de la vista producto {}",producto);
+		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
+		producto.setUsuario(u);
+		
+		productoService.guardar(producto);
+		return "redirect:/productos";
+	}
+	
 }
--- src/main/java/com/proyecto/ecommerce/service/ProductoService.java
+++ src/main/java/com/proyecto/ecommerce/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.proyecto.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.proyecto.ecommerce.model.Producto;
@@ -13,4 +14,6 @@ public interface ProductoService {
 	public void actualizar(Producto producto);
 	
 	public void borrar(Integer id);
+	
+	public List<Producto> mostrar();
 }
