0555bc24bba0def835fea96a4ba14f0d3ea67367
Fernando <lmartinezm013@gmail.com>
2023-07-15 19:37:15 -0500

    funcionalidad de las imagenes

--- src/main/java/com/proyecto/ecommerce/controler/ProductoController.java
+++ src/main/java/com/proyecto/ecommerce/controler/ProductoController.java
@@ -1,5 +1,6 @@
 package com.proyecto.ecommerce.controler;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import org.slf4j.*;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.proyecto.ecommerce.model.Producto;
 import com.proyecto.ecommerce.model.Usuario;
 import com.proyecto.ecommerce.service.ProductoService;
+import com.proyecto.ecommerce.service.UploadFileService;
 
 
 @Controller
@@ -21,6 +25,9 @@ import com.proyecto.ecommerce.service.ProductoService;
 public class ProductoController {
 	
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+	
+	@Autowired
+	private UploadFileService upload;
 	
 	@Autowired
 	private ProductoService productoService;
@@ -38,11 +45,15 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/save")
-	public String guardar(Producto producto) {
+	public String guardar(Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
 		LOGGER.info("Objeto de la vista producto {}",producto);
 		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
 		producto.setUsuario(u);
-		
+		//subida de imagenes
+		if (producto.getId()==null) { //si se crea el producto
+			String nombreImagen = upload.guardarImagen(file);
+			producto.setImagen(nombreImagen);
+		}
 		productoService.guardar(producto);
 		return "redirect:/productos";
 	}
@@ -59,14 +70,39 @@ public class ProductoController {
 	}
 	
 	@PostMapping("/update")
-	public String subir(Producto producto) {
+	public String subir(Producto producto,@RequestParam("img") MultipartFile file) throws IOException {
+		if (file.isEmpty()) { //edicion del producto pero no se cambia la imagen
+			Producto p = new Producto();
+			p = productoService.obtener(producto.getId()).get();
+			producto.setImagen(p.getImagen());
+		}else {//edicion del producto pero si se cambia la imagen
+			Producto p =new Producto();
+			p=productoService.obtener(producto.getId()).get();
+			
+			if (!p.getImagen().equals("default.jpg")) {//se eliminina solo si no es la imagen por defecto
+				upload.borrarimagen(p.getImagen());
+			}
+			
+			String nombreImagen = upload.guardarImagen(file);
+			producto.setImagen(nombreImagen);
+		}
+		
 		productoService.actualizar(producto);
 		return "redirect:/productos";
 	}
 	
 	@GetMapping("/delete/{id}")
 	public String borrar(@PathVariable Integer id) {
+		Producto p =new Producto();
+		p=productoService.obtener(id).get();
+		
+		if (!p.getImagen().equals("default.jpg")) {//se eliminina solo si no es la imagen por defecto
+			upload.borrarimagen(p.getImagen());
+		}
+		
 		productoService.borrar(id);
 		return "redirect:/productos";
 	}
+	
+	
 }
